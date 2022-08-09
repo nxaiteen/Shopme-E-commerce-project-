9b88a50e4e6cb7b753ce23af008f06d0dcc898b7
Nikita Romanov <nxaiteen19@mail.ru>
2022-08-09 13:25:33 +0300

    Code Update User Enabled Status

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,5 +1,6 @@
 package com.shopme.admin.user;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,5 +11,11 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
 	
+	//Используется для проверки существования пользователя по ID (исп. для удаления пользователя по ID)
 	public Long countById(Integer id);
+	
+	//Используется для обновления статуса enabled/disabled
+	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);
 }
