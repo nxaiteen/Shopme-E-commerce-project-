34d64a9a4ac8ded68ab8a247400f03053908f615
Nikita Romanov <nxaiteen19@mail.ru>
2022-08-12 13:13:27 +0300

    Code Filter Function for Users List

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,11 +11,15 @@ import org.springframework.data.repository.query.Param;
 import com.shopme.common.entity.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer>{
+	
 	@Query("SELECT u FROM User u WHERE u.email = :email")
 	public User getUserByEmail(@Param("email") String email);
 	
 	//Используется для проверки существования пользователя по ID (исп. для удаления пользователя по ID)
 	public Long countById(Integer id);
+	
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.email, ' ', u.firstname, ' ', u.lastname) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 	
 	//Используется для обновления статуса enabled/disabled
 	@Query("UPDATE User u SET u.enabled = ?2 WHERE u.id = ?1")
