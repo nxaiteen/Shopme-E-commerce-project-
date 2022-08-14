6583781ac6b8df88ade216968cb633a2cc98c298
Nikita Romanov <nxaiteen19@mail.ru>
2022-08-14 16:12:51 +0300

    Code Export Users to Excel

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/AbstractExporter.java
@@ -0,0 +1,26 @@
+package com.shopme.admin.user;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class AbstractExporter {
+	
+	public void setResponseHeader(HttpServletResponse response,
+			String contentType, String extension) throws IOException {
+		
+		DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+		String timestamp = dateFormatter.format(new Date());
+		String fileName = "users_" + timestamp + extension;
+		
+		response.setContentType(contentType);
+		
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=" + fileName;
+		response.setHeader(headerKey, headerValue);
+	}
+}
