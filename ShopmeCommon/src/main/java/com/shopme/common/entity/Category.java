245da3613c375317394a6b0576901b671f8b780f
Nikita Romanov <nxaiteen19@mail.ru>
2023-04-23 01:00:25 +0300

    Code Pagination, Sort and Filter for Products Page

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -32,6 +32,9 @@ public class Category {
 
 	private boolean enabled;
 
+	@Column(name = "all_parent_ids", length = 256, nullable = true)
+	private String allParentIDs;
+
 	@OneToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent;
@@ -178,4 +181,11 @@ public class Category {
 		return this.name;
 	}
 
+	public String getAllParentIDs() {
+		return allParentIDs;
+	}
+
+	public void setAllParentIDs(String allParentIDs) {
+		this.allParentIDs = allParentIDs;
+	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -23,7 +25,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.FileUploadUtil;
 import com.shopme.admin.brand.BrandService;
+import com.shopme.admin.category.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 import com.shopme.common.entity.product.Product;
 import com.shopme.common.entity.product.ProductImage;
 
@@ -33,12 +37,46 @@ public class ProductController {
 
 	@Autowired private ProductService productService;
 	@Autowired private BrandService brandService;
+	@Autowired private CategoryService categoryService;
 
 	@GetMapping("/products")
-	public String listAll(Model model) {
-		List<Product> listProducts = productService.listAll();
+	public String listFirstPage(Model model) {
+		return listByPage(1, model, "name", "asc", null, 0);
+	}
 
+	@GetMapping("/products/page/{pageNum}")
+	public String listByPage(
+			@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+			@Param("keyword") String keyword,
+			@Param("categoryId") Integer categoryId
+	) {
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword, categoryId);
+		List<Product> listProducts = page.getContent();
+
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+		long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		if (categoryId != null) model.addAttribute("categoryId", categoryId);
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
 		model.addAttribute("listProducts", listProducts);
+		model.addAttribute("listCategories", listCategories);
 
 		return "products/products";
 	}
@@ -78,14 +116,14 @@ public class ProductController {
 
 		saveUploadedImages(mainImageMultipart, extraImageMultiparts, savedProduct);
 
-		deleteExtraImagesWereRemovedOnForm(product);
+		deleteExtraImagesWeredRemovedOnForm(product);
 
 		ra.addFlashAttribute("message", "The product has been saved successfully.");
 
 		return "redirect:/products";
 	}
 
-	private void deleteExtraImagesWereRemovedOnForm(Product product) {
+	private void deleteExtraImagesWeredRemovedOnForm(Product product) {
 		String extraImageDir = "../product-images/" + product.getId() + "/extras";
 		Path dirPath = Paths.get(extraImageDir);
 
